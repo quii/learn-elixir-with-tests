a3872d8ed94fe703518dd582d665d7a281c858c5
Chris James <quii@hey.com>
2021-08-30 12:15:16 +0100

    maps

--- test/learn_elixir_with_tests_test.exs
+++ test/learn_elixir_with_tests_test.exs
@@ -20,7 +20,7 @@ defmodule LearnElixirWithTestsTest do
   end
 
   test "chaining is done with |>, the result is passed in as first arg to the next f" do
-    assert chaining == 7
+    assert chaining() == 7
   end
 
   test "working with tuples" do 
@@ -30,6 +30,22 @@ defmodule LearnElixirWithTestsTest do
 
     assert put_elem(person, 1, 38) == {"Chris", 38}
     assert person == {"Chris", 37} # put_elem does not modify, returns new version
+  end
+
+  test "working with maps" do
+    person = %{name: "Chris", age: 37} #if keys are atoms, there is shorthand
+    person2 = %{:name => "Ruth", :age => 36} 
+
+    assert person[:name] == "Chris"
+    assert person2[:age] == 36
+    assert person.name == "Chris" # again atoms are special
+    assert person2.name == "Ruth"
+
+    person2 = %{person2 | age: 37} # note, you cant add new fields
+    assert person2.age == 37
+
+    person2 = Map.put(person2, :salary, 100)
+    assert person2.salary == 100
   end
 
 end
